1818939e653997e6b0639afdc84831f1e3f5d007
Marcus Scholz <marcus.scholz@computerProfessionals.com.au>
2017-05-03 12:39:30 +1000

    .

--- OrderImportClasses/Log.cs
+++ OrderImportClasses/Log.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataModel;
+using System.Diagnostics;
 
 namespace OrderImportClasses
 {
@@ -11,15 +12,35 @@ namespace OrderImportClasses
     {
         public static void LogError(string psMessage, string psModule, Int32 piShipReqID = 0)
         {
-            ShippingRequestError E = new ShippingRequestError();// new ShippingRequestError();
-            E.DateCreated = System.DateTime.Now;
-            E.Module = psModule;
-            E.ErrorDetail = psMessage;
-            E.ShipReqID = piShipReqID;
-            using  (SCT db = new SCT()){
-                db.ShippingRequestErrors.Add(E);
-                db.SaveChanges();
-            }           
+            try {
+                ShippingRequestError E = new ShippingRequestError();// new ShippingRequestError();
+                E.DateCreated = System.DateTime.Now;
+                E.Module = psModule;
+                E.ErrorDetail = psMessage;
+                E.ShipReqID = piShipReqID;
+                using (SCT db = new SCT()) {
+                    db.ShippingRequestErrors.Add(E);
+                    db.SaveChanges();
+                }
+            } catch (Exception e)
+            {
+
+                LogErrorRaw("Error occurred when logging an error.  Original error: " + psModule + ": " + psMessage + " : " + e.ToString(), "Log.LogError");
+
+            }      
+        }
+
+        public static void LogErrorRaw(string psMessage, string psModule)
+        {
+                string sSource= "OrderImportClasses";
+                string sLog="Application";
+                string sEvent=psMessage;
+
+                if (!EventLog.SourceExists(sSource))                    
+                    EventLog.CreateEventSource(sSource, sLog);
+
+                EventLog.WriteEntry(sSource, sEvent, EventLogEntryType.Error);
+
         }
     }
 }
